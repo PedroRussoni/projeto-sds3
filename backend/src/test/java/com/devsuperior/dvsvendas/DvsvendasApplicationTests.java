44794b4b4b07bb31db075980a9955f810dd09cbe
PedroRussoni <pedrorussoni51@gmail.com>
2021-09-12 12:21:07 -0300

    Project Created

---
+++ backend/src/test/java/com/devsuperior/dvsvendas/DvsvendasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuperior.dvsvendas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DvsvendasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
